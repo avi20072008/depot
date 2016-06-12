8db8dc2d6662ad89c37d409932d5bd45ae68b6ca
Avinash Patil <meetjasper@gmail.com>
2016-06-12 17:13:30 -0500

    Initial Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  resources :line_items
+
+  resources :carts
+
   get 'store/index'
 
   resources :products
   root 'store#index'
+  match ':controller(/:action/(:id))', :via => [:get, :post, :patch]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
